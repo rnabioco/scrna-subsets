3c733cc97e7b99d72388d8b25c125719dbfc0373
Kent Riemondy <kent.riemondy@gmail.com>
2018-02-16 17:01:33 -0700

    make pipeline work with 10x and wafergen

--- bin/barcode_tag_bam.cpp
+++ bin/barcode_tag_bam.cpp
@@ -102,13 +102,26 @@ int hamming_distance(const std::string& fs, const std::string& ss){
 int main(int argc, char *argv[])
 {
   if (argc == 1) {
-    std::cerr << "Usage: %s <in.bam> <in.bcs> <out.bam \n" << argv[0] ;
+    std::cerr << "Usage: %s <in.bam> <in.bcs> <out.bam> <cbc_start> <cbc_end> <umi_start> <umi_end> \n" << argv[0] ;
     return 1;
-  } else if  (argc == 3) {
+  } else if  (argc != 8) {
     std::cerr << "input and output file names required" << std::endl ;
     return 1;
   }
-
+  
+  int cbc_start, cbc_end, umi_start, umi_end ;
+  cbc_start = std::stoi(argv[4]) ; 
+  cbc_end   = std::stoi(argv[5]) ;
+  umi_start = std::stoi(argv[6]) ;
+  umi_end   = std::stoi(argv[7]) ;
+  
+  
+  int cbc_start_idx = cbc_start - 1 ;
+  int umi_start_idx = umi_start - 1 ;
+  
+  int cbc_len = cbc_end - cbc_start_idx ;
+  int umi_len = umi_end - umi_start_idx ; 
+  
   //generate map of cell bcs and cell names
   std::string bcs = argv[2] ;
   auto bc_map = buildMap(bcs) ;
@@ -129,8 +142,8 @@ int main(int argc, char *argv[])
     char delim = ':';
     splitName(id, delim, id_elements) ; //split up name field
     auto read_seq = id_elements.back() ; //get seq
-    auto cbc = read_seq.substr(0, 11) ;
-    auto umi = read_seq.substr(11, 10) ;
+    auto cbc = read_seq.substr(cbc_start_idx, cbc_len) ;
+    auto umi = read_seq.substr(umi_start_idx, umi_len) ;
 
     //see if cell barcode is in map, if not take first one 1 hamming dist away
     // otherwise report unmatched
