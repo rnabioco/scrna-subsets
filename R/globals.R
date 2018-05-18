e0a67db394c2bec9d61ed5b5ec271cf52bc34dd9
Kent Riemondy <kent.riemondy@gmail.com>
2018-05-18 12:09:48 -0600

    various updates   - start tracking html   - add new analyses

--- R/globals.R
+++ R/globals.R
@@ -45,3 +45,115 @@ write_gztsv <- function(df, name, ...){
   readr::write_tsv(df, uncompressed_name, ...)
   system(paste0("gzip -f ", uncompressed_name))
 }
+
+
+plot_feature <- function(seurat_obj,
+                         ident = NULL,
+                         gene = NULL,
+                         plot_dat = NULL,
+                         plot_col = NULL,
+                         pt.size = 0.001,
+                         pt.alpha = 0.25,
+                         label_text = FALSE,
+                         label.size = 6,
+                         .cols = NULL,
+                         legend_names = NULL,
+                         cell_filter = NULL,
+                         palette_type = "cloupe",
+                         col_pal = "Reds",
+                         max_y = NULL,
+                         ...){
+  
+  mdata <- seurat_obj@meta.data %>% tibble::rownames_to_column("cell")
+  
+  tsne_dat <- seurat_obj@dr$tsne@cell.embeddings %>% 
+    as.data.frame() %>% 
+    tibble::rownames_to_column("cell")
+  
+  tsne_dat <- left_join(mdata, tsne_dat, by = "cell")
+  
+  if (!is.null(cell_filter)){
+    tsne_dat <- dplyr::filter(tsne_dat,
+                              cell %in% cell_filter)
+  }
+  
+  if (!is.null(gene)) {
+    gene_dat <- FetchData(seurat_obj, gene) %>% 
+      as.data.frame() %>% 
+      tibble::rownames_to_column("cell")
+    tsne_dat <- left_join(tsne_dat, gene_dat, by = "cell")
+  }
+  
+  if (!is.null(plot_dat)){
+    tsne_dat <- left_join(tsne_dat, plot_dat, by = "cell")
+  }
+  
+  if (!is.null(ident)){
+    color_aes <- ident
+  } else if (!is.null(plot_dat)){
+    color_aes <- plot_col
+  } else {
+    color_aes <- gene
+  }
+  
+  p <- ggplot(tsne_dat, 
+              aes(tSNE_1, tSNE_2)) +
+    geom_point(aes_string(color = color_aes),
+               size = pt.size,
+               alpha = pt.alpha)
+  
+  ## handle legend limit 
+  
+  if (is.null(max_y) & is.null(ident)) {
+    max_y <- c(0, max(tsne_dat[[color_aes]]))
+  } else if (!is.null(ident)){
+    max_y <- c(NA, NA)
+  }
+  
+  ## handle colors
+  if (is.null(.cols)){
+    if (palette_type == "viridis") {
+      p <- p + viridis::scale_color_viridis(discrete = F,
+                                   direction = -1,
+                                   option = col_pal,
+                                   limits = max_y)
+    } else if (palette_type == "brewer") {
+      p <- p + scale_color_distiller(limits = max_y,
+                                     palette = col_pal,
+                                     direction = 1)
+    } else if (palette_type == "cloupe") {
+      cols <- RColorBrewer::brewer.pal(11, "RdGy")[c(1:5, 7)]
+      
+      p <- p + scale_color_gradientn(limits = max_y,
+                                     colors = rev(cols))
+    }
+  }
+  p
+}
+
+
+#' fill a matrix with missing rownames present in 
+#' another matrix
+#' 
+#' @param xmat reference matrix from which to take rownames
+#' @param ymat matrix to fill
+#' @param fill_value value to fill values in ymat
+standardize_rows <- function(xmat,
+                             ymat,
+                             fill_value = 0L){
+  
+  stopifnot(all(colnames(xmat) == colnames(ymat)))
+  
+  ref_rows <- rownames(xmat)
+  ref_cols <- colnames(xmat)
+  ncols <- length(ref_cols)
+  rows_to_add <- setdiff(ref_rows, rownames(ymat))
+  zero_mat <- matrix(fill_value, 
+                     ncol = ncols, 
+                     nrow = length(rows_to_add),
+                     dimnames = list(rows_to_add, ref_cols))
+  
+  res <- rbind(ymat, 
+               zero_mat)
+  res
+}
